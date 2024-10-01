cc652433510805c511b23c66fa8048d1928babdd
Líberth Luquécio Lelis Cardoso <88115119+DrLiTH7@users.noreply.github.com>
2024-10-01 10:52:03 -0300

    Update exports.js

--- src/exports.js
+++ src/exports.js
@@ -1,4 +1,4 @@
-token = '8071848939:AAEu5OBn7h-90aT10iYYyC70eI9hZTRHWbs'; 
-path_ytdlp = '/home/lith/Documents/dev_all/FMP_Downloader/ytdlp/yt-dlp';
+token = '1234:seuTokenAqui'; // token do bot
+path_ytdlp = '/caminho/para/yt-dlp'; //recomendo adicionar ao PATH do sistema, então basta colocar o nome do arquivo aqui
 
 module.exports = {token, path_ytdlp}
